bb8edeaf6850a73d556a6a8ae4b34d67a1fbbd25
Sepsap <saputraseptian158@gmail.com>
2023-10-13 07:18:57 +0700

    Remove comments

--- myscript.js
+++ myscript.js
@@ -1,27 +1,11 @@
-/*write a program that take a user's input
- from three choices:
-rock, paper, and scissor
-and compare it to the com's output
-rock beats scissor, scissor beats paper, paper beats rock
-show the win/lose message */
-
-
-
-//user input one out of three: rock paper scissor
-//computer also output a choice out of those 
 function getComputerChoice() {
   options = ["rock", "paper", "scissor"];
   return options[Math.floor(Math.random() * options.length)];
 }
 
-//compare both choices
-//the rule is rock > scissor, scissor > paper, paper > rock
-//same choices will be tied
-//if the player win, message you won
-//if the player lose, message you lose
-
-
 function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt("Pick your choice");
+  computerSelection = getComputerChoice();
   if ((playerSelection == "rock" && computerSelection == "paper") 
     || (playerSelection == "paper" && computerSelection == "scissor") 
     || (playerSelection == "scissor" && computerSelection == "rock")) {
@@ -36,45 +20,7 @@ function playRound(playerSelection, computerSelection) {
 let playerSelection;
 let computerSelection;
 
-//play five games
 function game() {
-  let a = 0;
-  let b = 0;
-  if (playRound(playerSelection, computerSelection) == "You won!") {
-     a++;
-  } else if (playRound(playerSelection, computerSelection) == "Loser! bahahah") {
-     b++;
-  }
-
-  playerSelection = prompt("Pick your choice");
-  computerSelection = getComputerChoice();
-  console.log(computerSelection);
   console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt("Pick your choice");
-  computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt("Pick your choice");
-  computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt("Pick your choice");
-  computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt("Pick your choice");
-  computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
-  
-  console.log(a);
-  console.log(b);
-}
-console.log(game())
-//each games must be recorded, scored
-
-//whoever got the higher score wins
+} 
+console.log(game()) 
